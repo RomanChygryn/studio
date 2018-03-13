d5b5a46a5977819e0039a6f7bb76449d86d8ce96
RomanChygryn <r.chigrin@gmail.com>
2018-03-13 12:42:45 +0100

    add about section and style, add skills section, made mobile also

---
+++ app/js/progress.js
@@ -0,0 +1,14 @@
+var progressDesign = document.getElementById("progressDesign"),
+    progressWeb = document.getElementById("progressWeb"),
+    progressMarketing = document.getElementById("progressMarketing");
+
+    setTimeout(
+      function(){
+        progressDesign.style.width = "75%";
+        progressDesign.style.backgroundColor = "#10c9c3";
+        progressWeb.style.width = "90%";
+        progressWeb.style.backgroundColor = "#10c9c3";
+        progressMarketing.style.width = "65%";
+        progressMarketing.style.backgroundColor = "#10c9c3";
+      }
+    ,10);
