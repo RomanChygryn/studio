c5c9730e1d12095383ef56b9890dd950300280ec
RomanChygryn <r.chigrin@gmail.com>
2018-03-14 17:22:53 +0100

    add load more button and js

---
+++ app/js/loadmore.js
@@ -0,0 +1,11 @@
+var hidden = document.getElementById("galleryHidden");
+
+
+
+function loadMore() {
+  if (hidden.className === "gallery__hidden") {
+      hidden.className = "gallery__hidden--active";
+  } else {
+      hidden.className = "gallery__hidden";
+  }
+};
